ebb3aa72b3c8bb4727f392d4e3f360b238cab2c5
wushijie <380297880@qq.com>
2021-07-23 14:51:46 +0800

    1.0

---
+++ cmake-build-debug/CMakeFiles/DailyTraining.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DailyTraining.dir/src/main/main.cpp.obj"
+  "DailyTraining.exe"
+  "DailyTraining.exe.manifest"
+  "DailyTraining.pdb"
+  "libDailyTraining.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DailyTraining.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
