989833000a5cacbe8d50216f5a77a90a09002d11
wushijie <380297880@qq.com>
2021-08-11 20:15:40 +0800

    1.0

---
+++ src/day2/text4.cpp
@@ -0,0 +1,18 @@
+// 枚举
+// Created by wushijie on 2021/8/11.
+//
+
+#include <iostream>
+using namespace std;
+
+enum weekday{
+    s,m,t,w,thu,f,s1
+};
+
+int main(){
+
+
+
+    system("pause");
+    return 0;
+}
